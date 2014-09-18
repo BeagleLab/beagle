b21d5a0aaf788ae38cc4561415584dc5f37bac7e
Richard Littauer <richard.littauer@gmail.com>
2014-09-18 02:55:04 -0700

    Sidebar toggles correctly, browserify is now part of the pipeline

--- README.md
+++ README.md
@@ -2,3 +2,12 @@ beagle
 ======
 
 Navigating the sea of publications
+
+##Installation
+
+1. Git clone this repository.
+2. Run `browserify -t brfs ./main.js > bundle.js`.
+2. Open Chrome Settings > Extensions.
+2. Click 'Load Unpacked Extension'.
+3. Select the repository folder.
+4. Click on the icon in the search bar on any page.
---
+++ main.js
@@ -0,0 +1,33 @@
+
+var fs = require('fs');
+
+/*Handle requests from background.html*/
+function handleRequest(
+  //The object data with the request params
+  request, 
+  //These last two ones isn't important for this example, if you want know more about it visit: http://code.google.com/chrome/extensions/messaging.html
+  sender, sendResponse
+  ) {
+  if (request.callFunction == "toggleSidebar")
+    toggleSidebar();
+}
+chrome.extension.onRequest.addListener(handleRequest);
+
+/*Small function wich create a sidebar(just to illustrate my point)*/
+var sidebarOpen = false;
+function toggleSidebar() {
+  if(sidebarOpen) {
+    var el = document.getElementById('mySidebar');
+    el.parentNode.removeChild(el);
+    sidebarOpen = false;
+  }
+  else {
+    var sidebar = document.createElement('div');
+    sidebar.innerHTML = '<style>' + 
+      fs.readFileSync(__dirname + '/main.css', 'utf8') + '</style>';
+    sidebar.innerHTML += fs.readFileSync(__dirname + '/sidebar.html', 'utf8');
+    sidebar.id = "mySidebar";
+    document.body.appendChild(sidebar);
+    sidebarOpen = true;
+  }
+}
