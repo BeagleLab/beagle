0f58f40a9ab692e56cdaca0bd769c909f59396ea
Richard Littauer <richard.littauer@gmail.com>
2014-09-22 23:53:30 -0700

    Attempted to remove background files; breaks the system.

--- bundle.js
+++ bundle.js
@@ -1,15 +1,5 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 
-console.log('The beginning!');
-
-
-// var s = document.createElement('script');
-// // TODO: add "script.js" to web_accessible_resources in manifest.json
-// s.src = chrome.extension.getURL('injectedScript.js');
-// s.onload = function() {
-//     this.parentNode.removeChild(this);
-// };
-// (document.head||document.documentElement).appendChild(s);
 
 /*Handle requests from background.html*/
 function handleRequest(
@@ -34,11 +24,9 @@ function toggleSidebar() {
   else {
     var sidebar = document.createElement('div');
     sidebar.innerHTML = '<style>' + 
-      ".beagle-sidebar {\n  position:fixed;\n  top:0px;\n  right:0px;\n  width:30%;\n  height:100%;\n  background:white;\n  box-shadow:0 0 1em black;\n  z-index:999999;\n}\n" + '</style>';
+      ".beagle-sidebar {\n  background:white;\n  box-shadow:0 0 1em black;\n  color: #990;\n  height:100%;\n  position:fixed;\n  right:0px;\n  top:0px;\n  width:30%;\n  z-index:999999;\n}\n" + '</style>';
     sidebar.innerHTML += "<div class=\"beagle-sidebar\">\n    <h1>Hello</h1>\n    World!\n</div>";
     sidebar.id = "mySidebar";
-    // sidebar.parentNode.innerHTML += '';
-
     document.body.appendChild(sidebar);
     sidebarOpen = true;
   }
