dc54a4debb4c6438d7738cc6a0280ca2d6a3bff0
Richard Littauer <richard.littauer@gmail.com>
2014-12-04 17:33:25 -0500

    Added in buttons

--- js/views/socialsharing.jsx
+++ js/views/socialsharing.jsx
@@ -5,7 +5,12 @@ module.exports = React.createClass({
   render: function() {
     return (
       <div>
-        <p>This is an exmaple subtemplate that works when called through app.jsx</p>
+        <button>
+          <a href="mailto:?subject=Champion: This is an awesome Article">Champion this article</a>
+        </button>
+        <button>
+          <a href="mailto:?subject=Nominate: Anyone else think this is great?">Nominate this article</a>
+        </button>
       </div>
     )
   }
