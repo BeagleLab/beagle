5527a650daee07af8f47d7282d2bc33a08172fed
Richard Littauer <richard.littauer@gmail.com>
2014-12-19 13:24:31 -0800

    Changed panel to modal

--- js/main.js
+++ js/main.js
@@ -16,178 +16,178 @@ var sidebarOpen = false;
 
 // Handle requests from background.html
 function handleRequest(
-  // The object data with the request params
-  request,
-  // These last two ones isn't important for this example.
-  // If you want know more about it visit:
-  // http://code.google.com/chrome/extensions/messaging.html
-  sender, sendResponse
-  ) {
-  if (request.callFunction == "toggleSidebar") {
+	// The object data with the request params
+	request,
+	// These last two ones isn't important for this example.
+	// If you want know more about it visit:
+	// http://code.google.com/chrome/extensions/messaging.html
+	sender, sendResponse
+	) {
+	if (request.callFunction == "toggleSidebar") {
 
-    if (!sidebarOpen) {
-      // If the extension has specified new modules to load
-      var newModules = request.modules ? request.modules : null ;
+		if (!sidebarOpen) {
+			// If the extension has specified new modules to load
+			var newModules = request.modules ? request.modules : null ;
 
-      //Get the current list of used modules
-      chrome.storage.sync.get('modules', function(result){
+			//Get the current list of used modules
+			chrome.storage.sync.get('modules', function(result){
 
-        var modules = result;
+				var modules = result;
 
-        // If there are new modules, append them
-        if (newModules) {
+				// If there are new modules, append them
+				if (newModules) {
 
-          modules.dependencies = (modules.dependencies) ? modules.dependencies : [];
+					modules.dependencies = (modules.dependencies) ? modules.dependencies : [];
 
-          _.each(newModules, function(module) {
-            modules.dependencies.push(module);
-          });
+					_.each(newModules, function(module) {
+						modules.dependencies.push(module);
+					});
 
-          // If there is a change, set it.
-          chrome.storage.sync.set(modules);
-        }
+					// If there is a change, set it.
+					chrome.storage.sync.set(modules);
+				}
 
-        var options = {
-          "altmetrics": true
-        }
+				var options = {
+					"altmetrics": true
+				}
 
-        try {
-          if (!navigator.onLine) {
-            throw (new Error('You are offline!'))
-          } else if (!PDFJS) {
-            console.log('PDFJS failed to load.');
-            throw (new Error('Error with PDFJS'))
-            // buildView(modules, 'Error with PDFJS');
-          } else if (document.querySelector("body>embed[type='application/pdf']")) {
+				try {
+					if (!navigator.onLine) {
+						throw (new Error('You are offline!'))
+					} else if (!PDFJS) {
+						console.log('PDFJS failed to load.');
+						throw (new Error('Error with PDFJS'))
+						// buildView(modules, 'Error with PDFJS');
+					} else if (document.querySelector("body>embed[type='application/pdf']")) {
 
-            PDFJS.readPDF(window.location.href, options, function(err, altmetricsData) {
-              if (err !== null) {
-                throw (new Error('Could not read the PDF'))
-              }
+						PDFJS.readPDF(window.location.href, options, function(err, altmetricsData) {
+							if (err !== null) {
+								throw (new Error('Could not read the PDF'))
+							}
 
-              if (altmetricsData) buildView(modules, {
-                data: {
-                  altmetrics: altmetricsData,
-                }
-              })
-            });
-          } else {
-            console.log('Not a pdf.');
-            // console.log(window.location);
-            // TODO Add in the DOM here.
-            buildView(modules);
-          }
-        }
+							if (altmetricsData) buildView(modules, {
+								data: {
+									altmetrics: altmetricsData,
+								}
+							})
+						});
+					} else {
+						console.log('Not a pdf.');
+						// console.log(window.location);
+						// TODO Add in the DOM here.
+						buildView(modules);
+					}
+				}
 
-        catch (e) {
-          console.log(e.name, e.message)
-          buildView(modules, e.message)
-        }
-      });
-    } else {
-      var el = document.getElementById('beagle-sidebar');
-      el.parentNode.removeChild(el);
-      sidebarOpen = false;
-    }
-  }
+				catch (e) {
+					console.log(e.name, e.message)
+					buildView(modules, e.message)
+				}
+			});
+		} else {
+			var el = document.getElementById('beagle-sidebar');
+			el.parentNode.removeChild(el);
+			sidebarOpen = false;
+		}
+	}
 }
 
 // TODO Enable Static Assets to go to other Views besides SideBar
 function buildStaticAssets(modules, textInput){
-  var sidebar = document.createElement('div');
-  sidebar.id = "beagle-sidebar";
+	var sidebar = document.createElement('div');
+	sidebar.id = "beagle-sidebar";
 
-  sidebar.innerHTML = '<link href="https://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css" rel="stylesheet" />';
-  sidebar.innerHTML += '<link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">'
+	sidebar.innerHTML = '<link href="https://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css" rel="stylesheet" />';
+	sidebar.innerHTML += '<link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">'
 
-  // Start the CSS and HTML objects
-  var concatCSS = document.createElement('style');
+	// Start the CSS and HTML objects
+	var concatCSS = document.createElement('style');
 
-  // Get the global CSS
-  // May be better for this to be async. For now, there are no checs;
-  // it loads or not. The encoding ensures string return, not buffer.
-  // Using path.join(__dirname, './main.css', ...) will be more portable,
-  // but doesn't work in Chrome for some reason.
-  concatCSS.innerHTML += fs.readFileSync(__dirname + '/../build/bundle.min.css', 'utf8');
+	// Get the global CSS
+	// May be better for this to be async. For now, there are no checs;
+	// it loads or not. The encoding ensures string return, not buffer.
+	// Using path.join(__dirname, './main.css', ...) will be more portable,
+	// but doesn't work in Chrome for some reason.
+	concatCSS.innerHTML += fs.readFileSync(__dirname + '/../build/bundle.min.css', 'utf8');
 
-  var outerPane = document.createElement('div');
-  outerPane.id = 'react';
+	var outerPane = document.createElement('div');
+	outerPane.id = 'react';
 
-  // Grab CSS files from style module. CSS and HTML shouldn't be exported from
-  // other submodules, in order to make sure that everything is modular.
-  if (style) {
-    concatCSS.innerHTML += (style.css) ? style.css : '';
-  }
+	// Grab CSS files from style module. CSS and HTML shouldn't be exported from
+	// other submodules, in order to make sure that everything is modular.
+	if (style) {
+		concatCSS.innerHTML += (style.css) ? style.css : '';
+	}
 
-  // Mung it all together
-  sidebar.appendChild(concatCSS);
-  sidebar.appendChild(outerPane);
+	// Mung it all together
+	sidebar.appendChild(concatCSS);
+	sidebar.appendChild(outerPane);
 
-  return sidebar;
+	return sidebar;
 }
 
 function buildView(modules, textInput) {
-  textInput = (textInput !== undefined) ? textInput : null;
-  var sidebar = buildStaticAssets(modules, textInput);
-  document.body.appendChild(sidebar);
-  React.renderComponent(
-    App(textInput),
-    document.getElementById('react')
-  )
-  linkHandler()
-  sidebarOpen = true;
+	textInput = (textInput !== undefined) ? textInput : null;
+	var sidebar = buildStaticAssets(modules, textInput);
+	document.body.appendChild(sidebar);
+	React.renderComponent(
+		App(textInput),
+		document.getElementById('react')
+	)
+	linkHandler()
+	sidebarOpen = true;
 }
 
 // for ease of dev. (move these before shipping widely)
 var Alert = require('./components/alert.jsx')
-var AuthorPanel = require('./views/authorPanel.jsx')
+var AuthorModal = require('./views/authorModal.jsx')
 var Altmetrics = require('./views/altmetrics.jsx')
 var Champion = require('./components/champion.jsx')
 var Publication = require('./views/publication.jsx')
 var Graph = require('./views/graph.jsx')
 var AltGraph = require('./views/altGraph.jsx')
 var Tags = require('./components/tags.jsx')
-var TagsPanel = require('./views/tagsPanel.jsx')
+var TagsModal = require('./views/tagsModal.jsx')
 var TagsList = require('./components/tagsList.jsx')
 var TagsListWrapper = require('./components/tagsListWrapper.jsx')
 var Save = require('./components/save.jsx')
 var Cite = require('./components/cite.jsx')
 var Annotations = require('./views/annotations.jsx')
 var MSeven = require('./milestones/mseven.jsx')
-var GraphPanel = require('./views/graphPanel.jsx')
+var GraphModal = require('./views/graphModal.jsx')
 var PublicationsList = require('./components/publicationsList.jsx')
 var PublicationsListWrapper = require('./components/publicationsListWrapper.jsx')
-var CitePanel = require('./views/citePanel.jsx')
+var CiteModal = require('./views/citeModal.jsx')
 
 // check if we're loading in the browser as an extension
 if (chrome && chrome.runtime && chrome.runtime.onMessage) {
-  chrome.runtime.onMessage.addListener(handleRequest);
+	chrome.runtime.onMessage.addListener(handleRequest);
 } else {
 
-  // export some things on window for non-extension pages.
-  // Declare all modules you need here. See comments above.
-  window.bundle = {
-    React: React,
+	// export some things on window for non-extension pages.
+	// Declare all modules you need here. See comments above.
+	window.bundle = {
+		React: React,
 
-    Alert: Alert,
-    AltGraph: AltGraph,
-    Altmetrics: Altmetrics,
-    App: App,
-    AuthorPanel: AuthorPanel,
-    Champion: Champion,
-    Graph: Graph,
-    Publication: Publication,
-    Save: Save,
-    Tags: Tags,
-    TagsPanel: TagsPanel,
-    TagsList: TagsList,
-    TagsListWrapper: TagsListWrapper,
-    Cite: Cite,
-    Annotations: Annotations,
-    MSeven: MSeven, // This is just to show annotations in the sidebar, and shouldn't actually be necessary
-    GraphPanel: GraphPanel,
-    PublicationsList: PublicationsList,
-    PublicationsListWrapper: PublicationsListWrapper,
-    CitePanel: CitePanel
-  }
+		Alert: Alert,
+		AltGraph: AltGraph,
+		Altmetrics: Altmetrics,
+		App: App,
+		AuthorModal: AuthorModal,
+		Champion: Champion,
+		Graph: Graph,
+		Publication: Publication,
+		Save: Save,
+		Tags: Tags,
+		TagsModal: TagsModal,
+		TagsList: TagsList,
+		TagsListWrapper: TagsListWrapper,
+		Cite: Cite,
+		Annotations: Annotations,
+		MSeven: MSeven, // This is just to show annotations in the sidebar, and shouldn't actually be necessary
+		GraphModal: GraphModal,
+		PublicationsList: PublicationsList,
+		PublicationsListWrapper: PublicationsListWrapper,
+		CiteModal: CiteModal
+	}
 }
