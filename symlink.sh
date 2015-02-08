163078454015e764bfb0388187fd11f36851a2dd
Richard Littauer <richard.littauer@gmail.com>
2015-02-08 12:50:56 -0800

    Added clone and npm installs and echoes to symlink

--- symlink.sh
+++ symlink.sh
@@ -1,3 +1,18 @@
-cd ./node_modules
+cd ..
+printf 'cd to' && pwd
+echo 'Cloning BeagleLab/beagle-style'
+git clone git@github.com:BeagleLab/beagle-style.git
+cd beagle-style 
+echo 'Installing modules...'
+npm install
+cd ..
+echo 'Cloning BeagleLab/beagle-pdf'
+git clone git@github.com:BeagleLab/beagle-pdf.git
+cd beagle-pdf
+echo 'Installing modules...'
+npm install 
+echo 'Set up symlinks...'
+cd ../beagle/node_modules
+printf 'cd to' && pwd
 ln -s ../../beagle-style
 ln -s ../../beagle-pdf
