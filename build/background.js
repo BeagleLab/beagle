bd7729def58a23d2d1cf685fbbd68a8d3326b151
Richard Littauer <richard.littauer@gmail.com>
2015-02-08 22:22:17 -0800

    Added in current work using h chrome extension code

--- build/background.js
+++ build/background.js
@@ -1,15 +1,10 @@
-console.log( 'Background.html starting!' );
-	/*Put page action icon on all tabs*/
-	chrome.tabs.onUpdated.addListener(function(tabId) {
-		chrome.pageAction.show(tabId);
-	});
+(function () {
+  'use strict';
 
-	chrome.tabs.getSelected(null, function(tab) {
-		chrome.pageAction.show(tab.id);
-	});
 
-	/*Send request to current tab when page action is clicked*/
-	chrome.pageAction.onClicked.addListener(function(tab) {
+
+	// /*Send request to current tab when page action is clicked*/
+	function fxt(tab) {
 		chrome.tabs.getSelected(null, function(tab) {
 			chrome.tabs.sendMessage(
 				//Selected tab id
@@ -17,15 +12,63 @@ console.log( 'Background.html starting!' );
 				//Params inside a object data
 				{
 					callFunction: "toggleSidebar",
-					modules: [
-						'altmetrics'
-					]
+					modules: [  'altmetrics' ]
 				},
 				//Optional callback function
 				function(response) {
-					console.log(response);
+					alert('Chrome tabs message sent.')	
 				}
 			);
 		});
-	});
-console.log( 'Background.html done.' );
+	}
+
+	chrome.browserAction.onClicked.addListener(fxt);
+
+	// THIS BREAKS. WHY
+	// Context Menu
+	// chrome.contextMenus.create({
+	//    title: 'Beagle',
+	//    contexts: ['page'],
+	//    onclick: function (detail, tab) { fxt(tab) }
+	// });
+	// END BREAK
+
+  var browserExtension = new h.HypothesisChromeExtension({
+    chromeTabs: chrome.tabs,
+    chromeBrowserAction: chrome.browserAction,
+    extensionURL: function (path) {
+      return chrome.extension.getURL(path);
+    },
+    isAllowedFileSchemeAccess: function (fn) {
+      return chrome.extension.isAllowedFileSchemeAccess(fn);
+    },
+  });
+
+  browserExtension.listen(window);
+  chrome.runtime.onInstalled.addListener(onInstalled);
+  chrome.runtime.onUpdateAvailable.addListener(onUpdateAvailable);
+
+  // function onInstalled(installDetails) {
+  //   if (installDetails.reason === 'install') {
+  //     browserExtension.firstRun();
+  //   }
+
+  //   // We need this so that 3rd party cookie blocking does not kill us.
+  //   // See https://github.com/hypothesis/h/issues/634 for more info.
+  //   // This is intended to be a temporary fix only.
+  //   var details = {
+  //     primaryPattern: 'https://hypothes.is/*',
+  //     setting: 'allow'
+  //   };
+  //   chrome.contentSettings.cookies.set(details);
+  //   chrome.contentSettings.images.set(details);
+  //   chrome.contentSettings.javascript.set(details);
+
+  //   browserExtension.install();
+  // }
+
+  // function onUpdateAvailable() {
+  //   // TODO: Implement a "reload" notification that tears down the current
+  //   // tabs and calls chrome.runtime.reload().
+  // }
+})();
