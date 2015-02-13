914cd15fc6aea7fe983abe51d9938814fb284955
Richard Littauer <richard.littauer@gmail.com>
2015-02-12 17:55:18 -0800

    Added gulp commands related to auto building
    Specifically, this:
     * Browserifies background.js
     * Moves manifest.json from static into build
     * Moves the PDF.JS viewer (dirname: content/) into the build folder,
       while excluding it from the other gulp commands
     * Watches the manifest, PDF.js, and background files
    
    This closes #75

--- gulpfile.js
+++ gulpfile.js
@@ -20,21 +20,30 @@ var sourcemaps = require('gulp-sourcemaps')
 // var style = require('beagle-style')
 
 var paths = {
-  build: 'build/',
-  main: ['./js/main.js'],
-  js: ['js/**/*.js', 'js/**/*.jsx'],
-  img: ['static/**/*.png', 'static/**/*.jpg'],
-  static: ['static/**/*.css', 'static/**/*.js', '../beagle-style/style.min.css', 'examples/example.pdf'],
-  sass: ['scss/**/main.scss'],
-  iframeSass: ['scss/**/iframe.scss'],
-  html: ['html/**/*.html'],
+  "build": 'build/',
+  "main": ['./js/main.js'],
+  "background": ['./js/background.js'],
+  "js": ['js/**/*.js', 'js/**/*.jsx'],
+  "img": ['static/**/*.png', 'static/**/*.jpg', '!static/content/**'],
+  "static": [
+    'static/**/*.css', 
+    'static/**/*.js', 
+    '!static/content/**/*.*',
+    '../beagle-style/style.min.css', 
+    'examples/example.pdf'
+  ],
+  "sass": ['scss/**/main.scss'],
+  "iframeSass": ['scss/**/iframe.scss'],
+  "html": ['html/**/*.html'],
+  "manifest": ['static/manifest.json'],
+  "content": ['static/content/**/*']
 }
 
 gulp.task('clean', function(cb) {
   del([paths.build], cb)
 })
 
-gulp.task('js', function() {
+gulp.task('main.js', function() {
   return browserify(paths.main)
     .transform(reactify)
     .transform({global: true }, 'brfs')
@@ -48,9 +57,33 @@ gulp.task('js', function() {
     .pipe(gulp.dest('build/'))
 })
 
+gulp.task('background.js', function() {
+  return browserify(paths.background)
+    .transform(reactify)
+    .transform({global: true }, 'brfs')
+    .bundle()
+    .pipe(source('background.min.js'))
+    // .pipe(sourcemaps.init())
+    // .pipe(g_if('*.jsx', react()))
+    // .pipe(uglify())
+    // .pipe(concat('bundle.min.js'))
+    // .pipe(sourcemaps.write())
+    .pipe(gulp.dest('build/'))
+})
+
 gulp.task('static', function() {
   return gulp.src(paths.static)
     .pipe(gulp.dest('build/static/'))
+})
+
+gulp.task('move', function() {
+  return gulp.src(paths.manifest)
+    .pipe(gulp.dest('build'))
+})
+
+gulp.task('content', function() {
+  return gulp.src(paths["content"], { base: './static/content/'})
+    .pipe(gulp.dest('build/content'))
 })
 
 gulp.task('sass', function() {
@@ -96,7 +129,9 @@ gulp.task('server', function() {
 })
 
 gulp.task('watch', function() {
-  gulp.watch(paths.js, ['js'])
+  gulp.watch(paths.manifest, ['static'])
+  gulp.watch(paths.js, ['background.js'])
+  gulp.watch(paths.js, ['main.js'])
   gulp.watch(paths.css, ['static'])
   gulp.watch(paths.sass, ['sass'])
   gulp.watch(paths.iframeSass, ['iframeSass'])
@@ -104,5 +139,5 @@ gulp.task('watch', function() {
   gulp.watch(paths.html, ['html'])
 })
 
-gulp.task('bundle', ['static', 'sass', 'iframeSass', 'js', 'img', 'html'])
-gulp.task('default', ['watch', 'static', 'sass', 'iframeSass', 'js', 'img', 'html', 'server'])
+gulp.task('bundle', ['move', 'static', 'sass', 'iframeSass', 'main.js', 'background.js', 'img', 'html', 'content'])
+gulp.task('default', ['watch', 'move', 'static','sass', 'iframeSass', 'main.js', 'background.js', 'img', 'html', 'server'])
