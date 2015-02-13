6e3fac3090d14c3633b77c246ee302d211464a14
Richard Littauer <richard.littauer@gmail.com>
2015-02-13 13:45:08 -0800

    Removed onInstalled and onUpdated

--- js/background.js
+++ js/background.js
@@ -1,4 +1,3 @@
-(function () {
   'use strict';
 
 
@@ -47,30 +46,5 @@
   });
 
   browserExtension.listen(window);
-  chrome.runtime.onInstalled.addListener(onInstalled);
-  chrome.runtime.onUpdateAvailable.addListener(onUpdateAvailable);
 
-  // function onInstalled(installDetails) {
-  //   if (installDetails.reason === 'install') {
-  //     browserExtension.firstRun();
-  //   }
-
-  //   // We need this so that 3rd party cookie blocking does not kill us.
-  //   // See https://github.com/hypothesis/h/issues/634 for more info.
-  //   // This is intended to be a temporary fix only.
-  //   var details = {
-  //     primaryPattern: 'https://hypothes.is/*',
-  //     setting: 'allow'
-  //   };
-  //   chrome.contentSettings.cookies.set(details);
-  //   chrome.contentSettings.images.set(details);
-  //   chrome.contentSettings.javascript.set(details);
-
-  //   browserExtension.install();
-  // }
-
-  // function onUpdateAvailable() {
-  //   // TODO: Implement a "reload" notification that tears down the current
-  //   // tabs and calls chrome.runtime.reload().
-  // }
-})();
+  // TODO Look into onInstalled and onUpdated.
