1cbe1e462222bf4af1fc3fe1b50c7621aa8ab0d2
Richard Littauer <richard.littauer@gmail.com>
2015-03-03 23:49:14 -0800

    Store text selections with appropriate metadata
    Before, each selection was stored dumbly as 'text'. Now, it uses a unique ID for the pdf, as well as the url it came from and an individual hash. The hash might be better done using the text itself put through a function, but that might end up with similarities, so... here we are.

--- js/views/grabText.jsx
+++ js/views/grabText.jsx
@@ -4,6 +4,8 @@ var rangy = require('rangy')
 var level = require('level-browserify')
 var db = level('./mydb')
 var crypto = require('crypto')
+var url = require('../lib/url-checks')
+var pdfjs = require('beagle-pdf')
 
 var GrabText = React.createClass({
   displayName: 'GrabText',
@@ -13,9 +15,32 @@ var GrabText = React.createClass({
   handleClick: function (event) {
     this.setState({text: !this.state.text})
     var text = rangy.getSelection().getRangeAt(0)
-    db.put('text', text, function (err) {
+
+    pdfjs.getFingerprint(url.getPDFURL(window.location.href),
+      function setDocumentId(err, fingerprint) {
+        if (err) {
+          console.log('Could not properly get PDF fingerprint')
+          // throw (new Error('Could not get the PDF fingerprint'))
+        }
+
+        var selection = {
+          //  Should probably be called 'selection', not 'text'
+          'text': text,
+
+          // We could also use the text as a hash, too.
+          'id': crypto.randomBytes(20).toString('hex'),
+
+          'url': url.getPDFURL(window.location.href),
+
+          // This is canonically the PDF id; else, just the name of the url should work.
+          'document_id': (fingerprint) ? fingerprint : url.getPDFURL(window.location.href)
+        }
+      }
+    )
+
+    db.put(selection.id, selection, function (err) {
       if (err) return console.log('Ooops!', err) // some kind of I/O error
-      console.log('Saved', text)
+      console.log(selection)
     })
       // chrome.storage.sync.set({'value': text.startContainer.data}, function() {
       //   // Notify that we saved.
