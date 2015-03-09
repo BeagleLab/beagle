71ed766b88a5aa4756845cab66937a731d799f2c
Richard Littauer <richard.littauer@gmail.com>
2015-03-08 23:35:55 -0700

    Ignore Milestones folder and Standardization

--- gulpfile.js
+++ gulpfile.js
@@ -20,28 +20,28 @@ var sourcemaps = require('gulp-sourcemaps')
 // var style = require('beagle-style')
 
 var paths = {
-  "js": ['js/**/*.js', 'js/**/*.jsx'],
-  "img": ['static/**/*.png', 'static/**/*.jpg', '!static/content/**'],
-  "static": [
   'build': 'build/',
   'main': 'main.js',
   'jsPath': './js/',
   'background': 'background.js',
+  'js': ['js/**/*.js', 'js/**/*.jsx', '!js/milestones/**/*'],
+  'img': ['static/**/*.png', 'static/**/*.jpg', '!static/content/**'],
   'milestones': ['js/milestones/**/*.*'],
+  'static': [
     'static/**/*.css',
     'static/**/*.js',
     '!static/content/**/*.*',
     '../beagle-style/style.min.css',
     'examples/example.pdf'
   ],
-  "sass": ['scss/**/main.scss'],
-  "iframeSass": ['scss/**/iframe.scss'],
-  "html": ['html/**/*.html'],
-  "manifest": ['static/manifest.json'],
-  "content": ['static/content/**/*']
+  'sass': ['scss/**/main.scss'],
+  'iframeSass': ['scss/**/iframe.scss'],
+  'html': ['html/**/*.html'],
+  'manifest': ['static/manifest.json'],
+  'content': ['static/content/**/*']
 }
 
-gulp.task('clean', function(cb) {
+gulp.task('clean', function (cb) {
   del([paths.build], cb)
 })
 
@@ -81,22 +81,22 @@ gulp.task('background.js', function() {
 //     .pipe(gulp.dest('build/milestones/'))
 // })
 
-gulp.task('static', function() {
+gulp.task('static', function () {
   return gulp.src(paths.static)
     .pipe(gulp.dest('build/static/'))
 })
 
-gulp.task('move', function() {
+gulp.task('move', function () {
   return gulp.src(paths.manifest)
     .pipe(gulp.dest('build'))
 })
 
-gulp.task('content', function() {
-  return gulp.src(paths["content"], { base: './static/content/'})
+gulp.task('content', function () {
+  return gulp.src(paths['content'], { base: './static/content/'})
     .pipe(gulp.dest('build/content'))
 })
 
-gulp.task('sass', function() {
+gulp.task('sass', function () {
   return gulp.src(paths.sass)
     .pipe(sourcemaps.init())
       .pipe(sass())
@@ -106,7 +106,7 @@ gulp.task('sass', function() {
     .pipe(gulp.dest('build/'))
 })
 
-gulp.task('iframeSass', function() {
+gulp.task('iframeSass', function () {
   return gulp.src(paths.iframeSass)
     .pipe(sourcemaps.init())
       .pipe(sass())
@@ -116,20 +116,20 @@ gulp.task('iframeSass', function() {
     .pipe(gulp.dest('build/'))
 })
 
-gulp.task('img', function() {
+gulp.task('img', function () {
   return gulp.src(paths.img)
     .pipe(imagemin({optimizationLevel: 5}))
     .pipe(gulp.dest('build/images/'))
     // .pipe(connect.reload())
 })
 
-gulp.task('html', function() {
+gulp.task('html', function () {
   return gulp.src(paths.html)
     .pipe(gulp.dest('build/'))
 })
 
 // Here for allviews and milestones
-gulp.task('server', function() {
+gulp.task('server', function () {
   connect.server({
     root: 'build',
     fallback: 'build/index.html',
@@ -138,7 +138,7 @@ gulp.task('server', function() {
   })
 })
 
-gulp.task('watch', function() {
+gulp.task('watch', function () {
   gulp.watch(paths.manifest, ['static'])
   gulp.watch(paths.js, ['background.js'])
   gulp.watch(paths.js, ['main.js'])
