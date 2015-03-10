acb1a420f2058a56a91f7fdabcb0a3ac32cb547a
Richard Littauer <richard.littauer@gmail.com>
2015-03-09 19:52:49 -0700

    Moved pdfjs functions to beagle-pdf

--- js/views/grabText.jsx
+++ js/views/grabText.jsx
@@ -10,34 +10,6 @@ var pdfjs = require('beagle-pdf')
 var cesc = require('chrome-ext-screen-capture')
 var _ = require('lodash')
 
-function getHightlightCoords () {
-  var pageIndex = window.PDFViewerApplication.pdfViewer.currentPageNumber - 1
-  var page = window.PDFViewerApplication.pdfViewer.pages[pageIndex]
-  var pageRect = page.canvas.getClientRects()[0]
-  var selectionRects = window.getSelection().getRangeAt(0).getClientRects()
-  var viewport = page.viewport
-  var selected = _.map(selectionRects, function (r) {
-    return viewport.convertToPdfPoint(r.left - pageRect.left, r.top - pageRect.top).concat(
-       viewport.convertToPdfPoint(r.right - pageRect.left, r.bottom - pageRect.top))
-  })
-  return {page: pageIndex, coords: selected}
-}
-
-function showHighlight (selected) {
-  var pageIndex = selected.page
-  var page = window.PDFViewerApplication.pdfViewer.pages[pageIndex]
-  var pageElement = page.canvas.parentElement
-  var viewport = page.viewport
-  selected.coords.forEach(function (rect) {
-    var bounds = viewport.convertToViewportRectangle(rect)
-    var el = document.createElement('div')
-    el.setAttribute('style', 'position: absolute; background-color: rgba(238, 170, 0, .2);' +
-      'left:' + Math.min(bounds[0], bounds[2]) + 'px; top:' + Math.min(bounds[1], bounds[3]) + 'px;' +
-      'width:' + Math.abs(bounds[0] - bounds[2]) + 'px; height:' + Math.abs(bounds[1] - bounds[3]) + 'px;')
-    pageElement.appendChild(el)
-  })
-}
-
 var GrabText = React.createClass({
   displayName: 'GrabText',
   getInitialState: function () {
@@ -48,12 +20,12 @@ var GrabText = React.createClass({
     this.setState({text: !this.state.text})
 
     // Get the coordinates we need and the text itself
-    var pdfCoords = getHightlightCoords()
+    var pdfCoords = pdfjs.getHightlightCoords()
     var htmlCoords = document.getSelection().getRangeAt(0).getBoundingClientRect()
     var text = rangy.getSelection().toString()
 
     // Highlight the selected text
-    showHighlight(pdfCoords)
+    pdfjs.showHighlight(pdfCoords)
 
     // Take a screenshot
     cesc.takeScreenshot(function (canvas) {
