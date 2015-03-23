9bf10afdd6bfa54ec1c97c8090e84efa8794d677
Richard Littauer <richard.littauer@gmail.com>
2015-03-23 14:02:54 +0800

    Linted, changed comment patterns

--- js/app.jsx
+++ js/app.jsx
@@ -5,12 +5,12 @@ var Forms = require('./views/forms.jsx').Forms
 var Highlight = require('./views/highlight.jsx')
 var Panel = require('react-bootstrap').Panel
 var Publication = require('./views/publication.jsx')
-var RetrieveValue = require('./views/retrieveValue.jsx')
 var Screenshot = require('./views/screenshot.jsx')
 var Sidebar = require('./components/sidebar.jsx')
 
 // var Cite = require('./components/cite.jsx')
 // var Graph = require('./views/graph.jsx')
+// var RetrieveValue = require('./views/retrieveValue.jsx')
 // var Save = require('./components/save.jsx')
 // var SignOut = require('./components/signOut.jsx')
 // var Tags = require('./components/tags.jsx')
@@ -18,46 +18,46 @@ var Sidebar = require('./components/sidebar.jsx')
 
 module.exports = React.createClass({
 
-  render: function() {
+  render: function () {
     return (
 			<Sidebar staticPath={this.props.staticPath} >
 
-				{/* <Save /> */}
-
-				<Highlight location={this.props.location} fingerprint={this.props.fingerprint} />
+        <Highlight location={this.props.location} fingerprint={this.props.fingerprint} />
 
         <Screenshot fingerprint={this.props.fingerprint} location={this.props.location} />
 
-				<Forms fingerprint={this.props.fingerprint} />
+        <Forms fingerprint={this.props.fingerprint} />
 
-				<Accordion>
-					<Panel header="Publication Details" eventKey='2' activeKey={false}>
-						<Publication data={this.props.data.publication} />
-					</Panel>
-				</Accordion>
+        <Accordion>
+          <Panel header="Publication Details" eventKey='2' activeKey={false}>
+            <Publication data={this.props.data.publication} />
+          </Panel>
+        </Accordion>
 
-				{/* <Accordion>
-					<Panel header="Snippets">
-						<RetrieveValue fingerprint={this.props.fingerprint} data={this.props.data} />
-					</Panel>
-				</Accordion> */}
+        {/* <Accordion>
+          <Panel header="Snippets">
+            <RetrieveValue fingerprint={this.props.fingerprint} data={this.props.data} />
+          </Panel>
+        </Accordion>
 
-				{/* <Accordion>
-					<Panel header="Table of Contents" eventKey='3'>
-						<Toc data={this.props.data.publication.toc} />
-					</Panel>
-				</Accordion> */}
+        <Accordion>
+          <Panel header="Table of Contents" eventKey='3'>
+            <Toc data={this.props.data.publication.toc} />
+          </Panel>
+        </Accordion>
 
-				{/* <h6>Graph</h6>
-				<Graph data={this.props.data.publication} /> */}
+        <h6>Graph</h6>
+        <Graph data={this.props.data.publication} />
 
-				{/* <h6>Tags</h6>
-				<Tags data={this.props.data.publication.subjects} />
+        <h6>Tags</h6>
+        <Tags data={this.props.data.publication.subjects} />
 
-				<h6>Cite</h6>
-				<Cite /> */}
+        <h6>Cite</h6>
+        <Cite />
 
-				{/* <SignOut /> */}
+        <Save />
+
+        <SignOut /> */}
 			</Sidebar>
 		)
   }
--- js/components/sidebar.jsx
+++ js/components/sidebar.jsx
@@ -2,34 +2,34 @@ var React = require('react')
 var Alert = require('./alert.jsx')
 
 module.exports = React.createClass({
-	displayName: 'Sidebar',
-  getInitialState: function() {
+  displayName: 'Sidebar',
+  getInitialState: function () {
     var staticPath
     if (this.props.staticPath) {
       staticPath = this.props.staticPath + 'images/noun_11582.png'
     } else if (chrome && chrome.extension) {
-      staticPath = chrome.extension.getURL("images/noun_11582.png")
+      staticPath = chrome.extension.getURL('images/noun_11582.png')
     } else {
       staticPath = 'images/noun_11582.png'
     }
-
 
     return {
       'staticPath': staticPath
     }
   },
-	render: function() {
-		return (
-			<div className="scinav sidebar">
-        <div className="pane-bg glass"></div>
-				<div className="pane">
-          <img src={this.state.staticPath} alt='HMS Beagle' className='beagle-icon' />
-					<h2 className='beagle-header'>Beagle</h2>
 
-					<Alert data={this.props.data} />
-					{this.props.children}
-	      </div>
-	    </div>
-		)
-	}
+  render: function () {
+    return (
+      <div className="scinav sidebar">
+        <div className="pane-bg glass"></div>
+        <div className="pane">
+          <img src={this.state.staticPath} alt='HMS Beagle' className='beagle-icon' />
+          <h2 className='beagle-header'>Beagle</h2>
+
+          <Alert data={this.props.data} />
+          {this.props.children}
+        </div>
+      </div>
+    )
+  }
 })
