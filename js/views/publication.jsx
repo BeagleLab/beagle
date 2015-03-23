8e502e2673dda9ef2f0d54700814a41336faf9fc
Richard Littauer <richard.littauer@gmail.com>
2015-03-23 14:32:43 +0800

    Added dummy warning closes #119

--- js/views/publication.jsx
+++ js/views/publication.jsx
@@ -6,12 +6,22 @@ module.exports = React.createClass({
    data: React.PropTypes.object
   },
   render: function () {
+    var dummyWarning
+
+    if (this.props.data.id === '000000') {
+      var style = {color: 'red'}
+      dummyWarning = <p style={style}>Unable to find DOI: presenting sample data.</p>
+    }
+
     return (
+      <div>
+        { dummyWarning }
         <ul>
           <li id='title'>{this.props.data.title}</li>
           <li>{this.props.data.journal}</li>
           <li>{this.props.data.doi}</li>
         </ul>
+      </div>
     )
   }
 })
