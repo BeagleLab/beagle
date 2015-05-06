1e845743287a3cceb3b27f8d8ea3c8d7443f08cb
Richard Littauer <richard.littauer@gmail.com>
2015-05-06 16:00:55 +0700

    Removed hello-inject, moved to button #147

--- js/views/login.jsx
+++ js/views/login.jsx
@@ -14,9 +14,36 @@ var Login = React.createClass({
     }
 
     try {
-
       // OAauth Code goes here.
-      console.log('No logic currently on click.')
+      hello('google').login().then(function () {
+        console.log("You're logged in!")
+      })
+
+      hello.init({
+        google: '832850147593-mg2vg2j8j65t3djpsifme57pljgimbl9.apps.googleusercontent.com'
+      }, {redirect_uri: 'http://www.google.com/robots.txt'})
+
+      // setInterval(function () {
+      //   var gl = hello('google').getAuthResponse()
+      //   console.log('gl', gl)
+      // }, 3000)
+
+      // hello.on('auth.login', function(auth){
+
+      //   console.log('Auth', auth)
+
+      //   // Call user information, for the given network
+      //   hello( auth.network ).api( '/me' ).then( function(r){
+      //     // Inject it into the container
+      //     var label = document.getElementById( 'profile_' + auth.network );
+      //     if (!label){
+      //       label = document.createElement('div');
+      //       label.id = 'profile_' + auth.network;
+      //       document.getElementById('profile').appendChild(label);
+      //     }
+      //     label.innerHTML = '<img src="' + r.thumbnail + '" /> Hey ' + r.name;
+      //   });
+      // });
 
       // style.color = 'green'
       // this.setState({success: <p style={style}>You are now logged in!</p>})
