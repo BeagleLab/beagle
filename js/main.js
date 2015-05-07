90a691479c6fb23cedc1ba11a6e6270504760286
Richard Littauer <richard.littauer@gmail.com>
2015-05-07 14:15:05 +0700

    Trying to work the OAuth using issue #55 to guide #133

--- js/main.js
+++ js/main.js
@@ -13,6 +13,7 @@ var url = require('./lib/url-checks')
 var PDFJS = require('beagle-pdf')
 var sidebarOpen = false
 var sidebarId = 'beagle-sidebar'
+var $ = require('jquery')
 
 // var level = require('level-browserify')
 var PouchDB = require('pouchdb')
--- js/views/login.jsx
+++ js/views/login.jsx
@@ -1,3 +1,4 @@
+/*globals OAuth */
 'use strict'
 
 var React = require('react')
@@ -14,14 +15,18 @@ var Login = React.createClass({
     }
 
     try {
-
       // OAauth Code goes here.
-      console.log('No logic currently on click.')
+      OAuth.initialize('IHLK6uDxpnuH1S71dCwbf30bjBM')
+      OAuth.popup('twitter', function (e, r) {
+        // to use r.get etc. you need to have jquery included at this point
+        r.get('/1.1/account/verify_credentials.json').done(function (data) {
+          console.log('Data', data)
+        })
+      })
 
       // style.color = 'green'
       // this.setState({success: <p style={style}>You are now logged in!</p>})
     } catch (e) {
-
       style.color = 'red'
       this.setState({success: <p style={style}>There was an error.</p>})
       // console.log('It looks like you are traversing an embedded PDF.')
