a47a92d6973a15247a9f0fc4c521764e3ccb4ca6
Richard Littauer <richard.littauer@gmail.com>
2015-05-11 19:19:16 +0700

    Standardized the utility function #163

--- js/utilities/csv.jsx
+++ js/utilities/csv.jsx
@@ -1,34 +1,34 @@
 /** @jsx React.DOM */
-var React = require('react');
+var React = require('react')
 
-var Util = {};
+var Util = {}
 
 Util.csv = React.createClass({
 	displayName: 'csv',
 	render: function () {
-		var items = {};
+    var items = {}
 
-		var length = this.props.data.length;
+    var length = this.props.data.length
 
-		var field = this.props.field;
+    var field = this.props.field
 
-		this.props.data.forEach(function(result, i) {
-			// If result.id can look like a number (consider short hashes), then
-			// object iteration order is not guaranteed. In this case, we add a prefix
-			// to ensure the keys are strings.
-			if (i+1 !== length) {
-				items['result-' + i] = <span>{result[field]}, </span>
-			} else {
-				items['result-' + i] = <span>{result[field]} </span>
-			}
-		});
+    this.props.data.forEach(function (result, i) {
+      // If result.id can look like a number (consider short hashes), then
+      // object iteration order is not guaranteed. In this case, we add a prefix
+      // to ensure the keys are strings.
+      if (i + 1 !== length) {
+        items['result-' + i] = <span>{result[field]}, </span>
+      } else {
+        items['result-' + i] = <span>{result[field]} </span>
+      }
+    })
 
-		return (
-				<span>
-					{items}
-				</span>
-		);
-	}
-});
+    return (
+      <span>
+        {items}
+      </span>
+    )
+  }
+})
 
-module.exports = Util.csv;
+module.exports = Util.csv
