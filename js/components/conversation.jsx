40947c0f60fc5b2713011ba379bebc7995be3259
Richard Littauer <richard.littauer@gmail.com>
2015-05-14 07:57:48 +0700

    Added user bar component #172

---
+++ js/components/conversation.jsx
@@ -0,0 +1,31 @@
+var React = require('react')
+var UserBar = require('./userBar.jsx')
+
+var text = 'Realism dolphin sentient artisanal plastic youtube franchise Kowloon numinous pen bridge meta-geodesic tower receding. Convenience store nano-dome 3D-printed bicycle shanty town claymore mine 8-bit. Realism rebar katana paranoid marketing smart-faded systemic skyscraper nano-shrine girl Tokyo urban franchise. Wristwatch dolphin ablative shoes neural voodoo god shrine math. Sunglasses A.I. assassin sub-orbital-ware-space assault Shibuya cardboard skyscraper geodesic crypto-spook kanji. '
+
+var Conversation = React.createClass({
+  displayName: 'Conversation',
+  render: function () {
+    var conversationStyle = {
+      margin: '5px 0px',
+      border: '1px solid #999',
+      borderLeft: '3px solid #AE8DC7',
+      borderRadius: '2px'
+    }
+
+    var textStyle = {
+      padding: '5px'
+    }
+
+    return (
+      <div style={conversationStyle}>
+        <UserBar user={this.props.user} />
+
+        <p style={textStyle}>{text}</p>
+
+      </div>
+    )
+  }
+})
+
+module.exports = exports = Conversation
