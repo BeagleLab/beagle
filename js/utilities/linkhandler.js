60fec3f2e4a824cce73fa08221cc404bcd2d2188
Richard Littauer <richard.littauer@gmail.com>
2015-06-02 16:25:33 +0800

    Linted linkhandler, changed reserved static var

--- js/utilities/linkhandler.js
+++ js/utilities/linkhandler.js
@@ -2,25 +2,28 @@ var Router = require('react-router')
 var $ = require('jquery')
 module.exports = linkHandler
 
-function linkHandler()  {
+function linkHandler () {
 
   var external = /^(https?:)?\/\//i
-  var static = /^\/static\//i
+  // static is reserved
+  var staticPath = /^\/static\//i
 
-  $(document.body).on('click', 'a', function(event) {
+  $(document.body).on('click', 'a', function (event) {
 
     var href = $(this).attr('href')
 
     // pass through if external or static
-    if (external.test(href) || static.test(href))
+    if (external.test(href) || staticPath.test(href)) {
       return
+    }
 
     // bail if already defaultPrevented
-    if (event.defaultPrevented)
+    if (event.defaultPrevented) {
       return
+    }
 
     Router.transitionTo(href)
     event.preventDefault()
     return false
-  });
+  })
 }
