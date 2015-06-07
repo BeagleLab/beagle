07229af043a8754a3fb8016e95d42c913c34cf27
Richard Littauer <richard.littauer@gmail.com>
2015-06-07 21:48:43 +0700

    Add test for newAccount #181

--- spec/test.js
+++ spec/test.js
@@ -7,3 +7,15 @@ describe('The schema', function () {
     expect(typeof (schema.newID()) === 'string').toBe(true)
   })
 })
+
+describe('Schema.newAccount', function () {
+  it('will check if name is valid', function () {
+    expect(schema.newAccount(10, 'test@test.com')).toBe(null)
+  })
+
+  // Will check if email is valid
+  // Will check if email exists
+  // Will check if name exists
+  // Will not create a new account if a user exists
+  // Will create a new account if a user does not exist
+})
