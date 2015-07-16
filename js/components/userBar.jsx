8b11c510b7b8ec986be5b21fd7b6059a8e6b8eb3
Richard Littauer <richard.littauer@gmail.com>
2015-07-16 13:13:13 -0400

    Conditionally display userBar if author is not logged in

--- js/components/userBar.jsx
+++ js/components/userBar.jsx
@@ -2,34 +2,6 @@ var React = require('react')
 var _ = require('lodash')
 var schema = require('../data/schema.js')
 
-var conversation = {
-  date: '10 minutes ago'
-}
-
-var imgStyle = {
-  margin: '10px 10px 5px 10px',
-  float: 'left',
-  width: 32,
-  height: 32
-}
-
-var nameStyle = {
-  paddingTop: '7px',
-  fontSize: '14px',
-  fontWeight: '600',
-  marginBottom: '3px'
-}
-
-var secondaryTextStyle = {
-  color: 'gray',
-  textDecoration: 'underline'
-}
-
-var iconStyle = {
-  // float: 'right'
-  color: 'gray'
-}
-
 var UserBar = React.createClass({
   displayName: 'User Bar',
   propTypes: {
@@ -37,23 +9,26 @@ var UserBar = React.createClass({
    secondaryText: React.PropTypes.string,
    conversation: React.PropTypes.participants
   },
-  // componentWillMount: function () {
-  //   // TODO Get users who exist from DB
-  //   _.each(this.props.participants, function (participant) {
-  //     schema.getUserDetails(participant).then(function (response) {
-  //       console.log('response', response)
-  //     }).catch(function (err) {
-  //       console.log('err', err)
-  //     })
-  //   })
-  // },
+  componentWillMount: function () {
+    if (this.props.conversation.author === this.props.account.userId) {
+      this.setState({author: this.props.account.userId})
+    }
+    // TODO Get users who exist from DB
+    // _.each(this.props.participants, function (participant) {
+    //   schema.getUserDetails(participant).then(function (response) {
+    //     console.log('response', response)
+    //   }).catch(function (err) {
+    //     console.log('err', err)
+    //   })
+    // })
+  },
   shimObjectToArray: function (object) {
     var arr = []
     if (object) {
       for (var key in object) {
         if (object.hasOwnProperty(key)) {
           // TODO This is stateful. Factor out.
-          if (key !== this.props.account.email) {
+          if (key !== this.props.conversation.author) {
             arr.push([key, object[key]])
           }
         }
@@ -64,10 +39,34 @@ var UserBar = React.createClass({
   render: function () {
 
     var secondaryText
+    var imgStyle = {
+      margin: '10px 10px 5px 10px',
+      float: 'left',
+      width: 32,
+      height: 32
+    }
 
-    if (this.props.secondaryText === 'date') {
+    var nameStyle = {
+      paddingTop: '7px',
+      fontSize: '14px',
+      fontWeight: '600',
+      marginBottom: '3px'
+    }
+
+    var secondaryTextStyle = {
+      color: 'gray',
+      textDecoration: 'underline'
+    }
+
+    var iconStyle = {
+      // float: 'right'
+      color: 'gray',
+      paddingLeft: 5
+    }
+
+    if (this.props.secondaryText === 'date' && this.props.conversation) {
       secondaryText = (<p style={secondaryTextStyle} >
-        {conversation.date}
+        {this.props.conversation.date}
       </p>)
     } else if (this.props.secondaryText === 'email') {
       secondaryText = (<p style={secondaryTextStyle} >
@@ -85,18 +84,37 @@ var UserBar = React.createClass({
       }
     }
 
-    return (
-      <div>
+    let authorDiv
+
+    if (this.props.conversation.author === this.props.account.userId) {
+      authorDiv = (
         <div className={{}}>
-          <img src={this.props.account.avatar} style={imgStyle} />
+          {
+            (this.props.account.avatar) ?
+            <img src={this.props.account.avatar} style={imgStyle} /> :
+            null
+          }
 
           <p style={nameStyle} >{this.props.account.name} <i style={iconStyle} className='fa fa-bolt'></i> Author:</p>
           {secondaryText}
         </div>
+      )
+    } else {
+      authorDiv = (
+        <div className={{}}>
+          <p style={nameStyle} ><i style={iconStyle} className='fa fa-bolt'></i> Author: {this.props.conversation.author}</p>
+          {secondaryText}
+        </div>
+      )
+    }
+
+    return (
+      <div>
+        {authorDiv}
         {this.shimObjectToArray(this.props.conversation.participants).map(function (participant) {
-            return (
-              <p style={{padding: '0px 5px'}}>{permIcons(participant[1])} {participant[0]}</p>
-            )
+          return (
+            <p style={{padding: '0px 5px'}}>{permIcons(participant[1])} {participant[0]}</p>
+          )
         })}
       </div>
     )
